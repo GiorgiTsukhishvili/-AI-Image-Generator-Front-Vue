828683fc000ca6bcdc30966412c4c8f65251babe
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-07-08 18:07:48 +0400

    feat: add like and comment icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,2 +1,4 @@
 export { default as CloseIcon } from "./CloseIcon.vue";
 export { default as ArrowRightIcon } from "./ArrowRightIcon.vue";
+export { default as LikeIcon } from "./LikeIcon.vue";
+export { default as CommentIcon } from "./CommentIcon.vue";
--- src/services/blogCRUD.js
+++ src/services/blogCRUD.js
@@ -1,3 +1,5 @@
 import { axios } from "./index";
 
 export const getAllBlogs = async () => await axios.get("/api/blog");
+
+export const getSingleBlog = async (id) => await axios.get(`/api/blog/${id}`);
