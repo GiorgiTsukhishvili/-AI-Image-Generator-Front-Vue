35a10916a602dee9a5ecd90b5223352e56b58e89
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-07-09 23:45:19 +0400

    feat: add single blog page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { HomeView } from "@/views";
+import { HomeView, SingleBlog } from "@/views";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +8,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/blog/:id",
+      name: "blog",
+      component: SingleBlog,
     },
   ],
 });
--- src/views/index.js
+++ src/views/index.js
@@ -1,1 +1,2 @@
 export { default as HomeView } from "./HomeView.vue";
+export { default as SingleBlog } from "./SingleBlog.vue";
