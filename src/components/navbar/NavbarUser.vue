a3d96cb4e1c747d6aa8c61b657fabca5fbc15569
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-08-01 17:16:01 +0400

    feat: add user navbar dropdown

--- src/components/navbar/NavbarUser.vue
+++ src/components/navbar/NavbarUser.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    <button @click="logout">logout</button>
+  <div
+    @focus="() => (dropdown = true)"
+    @focusout="() => (dropdown = false)"
+    tabindex="0"
+    class="relative"
+  >
+    <img
+      :src="props.userInfo.user.image"
+      :alt="props.userInfo.user.name + ' image'"
+      class="w-16 h-16 rounded-full cursor-pointer"
+    />
+
+    <div
+      class="w-[300px] rounded-md z-50 px-6 py-6 bg-white absolute duration-500 origin-top top-24 shadow-xl right-0"
+      :style="{ transform: dropdown ? 'scaleY(1)' : 'scaleY(0)' }"
+    >
+      <button @click="logout" class="text-xl font-semibold">Logout</button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 import { logoutUser, getCSRF } from "@/services";
 
 const props = defineProps({ userInfo: { required: true } });
+
+const dropdown = ref(false);
 
 const logout = async () => {
   try {
