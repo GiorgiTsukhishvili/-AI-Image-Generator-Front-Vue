d9723e47f5d5c7543bf201294336edb4d3619217
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-08-27 20:57:35 +0400

    feat: add unsubscribe button to user subscribers page

--- src/views/UserSubscriptions.vue
+++ src/views/UserSubscriptions.vue
@@ -6,21 +6,30 @@
           My Subscriptions:
         </h1>
         <ul class="mt-16 flex flex-wrap gap-20">
-          <RouterLink
+          <li
             v-for="subscriber in subscribers"
             :key="subscriber.id"
-            :to="{ name: 'user', params: { name: subscriber.user.name } }"
             class="flex flex-col items-center"
           >
-            <img
-              :src="subscriber.user.image"
-              :alt="subscriber.user.name + ' image'"
-              class="w-40 h-40 object-cover rounded-full"
-            />
-            <h2 class="mt-6 font-semibold text-center md:text-3xl text-2xl">
-              {{ subscriber.user.name }}
-            </h2>
-          </RouterLink>
+            <RouterLink
+              :to="{ name: 'user', params: { name: subscriber.user.name } }"
+            >
+              <img
+                :src="subscriber.user.image"
+                :alt="subscriber.user.name + ' image'"
+                class="w-40 h-40 object-cover rounded-full"
+              />
+              <h2 class="mt-6 font-semibold text-center md:text-3xl text-2xl">
+                {{ subscriber.user.name }}
+              </h2>
+            </RouterLink>
+            <button
+              @click="() => subscribeOrUnsubscribe(subscriber.user.id)"
+              class="mt-4 font-normal text-xl"
+            >
+              Unsubscribe
+            </button>
+          </li>
         </ul>
       </div>
     </WrapperComponent>
@@ -30,18 +39,32 @@
 <script setup>
 import { useUserStore } from "@/stores";
 
+import { ref, watchEffect } from "vue";
+
 import { WrapperComponent } from "@/components";
+
+import { userSubscribes, getCSRF, addSubscribe } from "@/services";
 
 const user = useUserStore();
 const subscribers = ref([]);
-
-import { userSubscribes } from "@/services";
-import { ref, watchEffect } from "vue";
 
 const userSubscribers = async () => {
   const data = await userSubscribes();
   console.log(data.data.subscribers);
   subscribers.value = data.data.subscribers;
+};
+
+const subscribeOrUnsubscribe = async (unsubscribeId) => {
+  try {
+    await getCSRF();
+    await addSubscribe({ subscribed_to: unsubscribeId });
+
+    subscribers.value = subscribers.value.filter(
+      (el) => el.user.id !== unsubscribeId
+    );
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 watchEffect(() => {
