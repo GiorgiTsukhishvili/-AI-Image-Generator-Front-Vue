6e3054b88106a2333277d3954ad6b3b4459d3d6e
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-10-14 17:17:39 +0400

    feat: add notification dropdown

--- src/components/user/UserNotification.vue
+++ src/components/user/UserNotification.vue
@@ -11,6 +11,12 @@
       {{ notificationNumber }}
     </div>
   </div>
+
+  <div
+    class="md:w-[300px] gap-5 flex flex-col md:justify-start justify-center md:items-start items-center md:h-auto h-screen w-screen rounded-md z-50 px-6 py-6 bg-white md:absolute fixed duration-500 origin-top md:top-24 top-0 shadow-xl right-0"
+    :style="{ transform: isNotificationOpen ? 'scaleY(1)' : 'scaleY(0)' }"
+    @mouseleave="() => (isNotificationOpen = false)"
+  ></div>
 </template>
 
 <script setup>
