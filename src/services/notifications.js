0b2b8725d03498eab563653df1bb124c86ae6e5f
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-11-19 17:22:57 +0400

    feat: add notification marking as read service

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -2,3 +2,6 @@ import { axios } from "./index";
 
 export const getAllNotifications = () =>
   axios.get("/api/notifications/user-notifications");
+
+export const markNotificationRead = (ids) =>
+  axios.post("/api/notifications/mark", { params: { ids } });
